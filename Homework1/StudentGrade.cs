d7aeb0408a2b8c6b621955bf3ee21983e67e3ee9
Elizabeth <e.haskins@ou.edu>
2019-09-06 13:18:11 -0500

    Add project files.

---
+++ Homework1/StudentGrade.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class StudentGrade
+    {
+        static void Main(string[] args)
+        {
+            //Elizabeth Haskins
+            //Declare Variables
+            string firstname;
+            string lastname;
+            string studentID;
+            string participationAsString;
+            string quizAsString;
+            string homeworkAsString;
+            string midtermAsString;
+            string finalAsString;
+            double homework, participation, quizzes, midterm, final, finalGrade;
+
+            //Get input from user
+            Console.WriteLine("What is your first name?");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("What is your student id?");
+            studentID = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            homeworkAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for participation?");
+            participationAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall perventage grade for quizzes?");
+            quizAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            midtermAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            finalAsString = Console.ReadLine();
+
+            //Convert Strings to number data types
+            homework = Convert.ToDouble(homeworkAsString);
+            participation = Convert.ToDouble(participationAsString);
+            quizzes = Convert.ToDouble(quizAsString);
+            midterm = Convert.ToDouble(midtermAsString);
+            final = Convert.ToDouble(finalAsString);
+
+            finalGrade = (homework + participation + quizzes + midterm + final) / 5;
+
+            //Output 
+            string sentence = firstname + " " + lastname + " (" + studentID + "), " + "your final grade is "
+                + (finalGrade / 100).ToString("P");
+
+            Console.WriteLine(sentence);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SaleTotal/SaleTotal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaleTotal
+{
+    static class SaleTotal
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Elizabeth Haskins
+            //Define Variables
+            string product;
+            string ticketnumAsString;
+            string ticketpriceAsString;
+            double numTicket, numTicketPrice, subtotal, total;
+
+            //Get user input
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            product = Console.ReadLine();
+
+            Console.WriteLine("How Many Football Ticket's do you want to buy?");
+            ticketnumAsString = Console.ReadLine();
+
+            Console.WriteLine("What is the price for each Football Ticket?");
+            ticketpriceAsString = Console.ReadLine();
+
+            //Convert strings to number data types
+            numTicket = Convert.ToDouble(ticketnumAsString);
+
+            numTicketPrice = Convert.ToDouble(ticketpriceAsString);
+
+            subtotal = numTicket * numTicketPrice;
+
+            double salestax = 0.085;
+            salestax = subtotal * salestax;
+
+            total = subtotal + salestax;
+
+            string subtotalSentence = "Your subtotal for your bill is " + subtotal.ToString("C2");
+            string salestaxSentence = "Your sales tax for your bill is " + salestax.ToString("C2");
+            string totalSentence = "Your total for your bill is " + total.ToString("C2");
+
+            //Output
+
+            Console.WriteLine(subtotalSentence);
+            Console.WriteLine(salestaxSentence);
+            Console.WriteLine(totalSentence);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
